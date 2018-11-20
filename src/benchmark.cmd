e1bccebac161f5608f15f35037c3b09f7f1f0a3a
Kimserey <kimserey.lam@groomgy.com>
2018-11-20 17:29:48 +0000

    remove cmd

--- src/benchmark.cmd
+++
@@ -1,2 +0,0 @@
-dotnet .\VegetaTargetGenerator\bin\Debug\netcoreapp2.1\VegetaTargetGenerator.dll %1 %2 | C:\Tools\vegeta\vegeta.exe attack -rate=%1 -duration=%2s -format=json > results.bin
-C:\Tools\vegeta\vegeta.exe plot results.bin > results.html
